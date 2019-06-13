b59920454b5520794808cea839b27e1615795498
imos <github@imoz.jp>
2019-06-13 10:02:45 +0900

    Upgrade protobuf version.

--- proto/build.rs
+++ proto/build.rs
@@ -4,6 +4,6 @@ fn main() {
     let proto_root = ".";
     println!("cargo:rerun-if-changed={}", "*.proto");
     protoc_grpcio::compile_grpc_protos(
-        &["data.proto"], &[""], &"src")
+        &["data.proto"], &[""], &"src", None)
         .expect("Failed to compile gRPC definitions!");
 }
