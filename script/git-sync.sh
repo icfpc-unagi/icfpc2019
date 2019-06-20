469f6019313cc801c8b27ac671361003f75481e3
imos <github@imoz.jp>
2019-06-20 11:36:29 +0900

    Update git-sync.

--- script/git-sync.sh
+++ script/git-sync.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-{
-  sleep 10
-  echo 'HTTP/1.0 200 OK'
-  echo 'Content-Type: text/html'
-  echo
-  echo 'OK'
-} | nc -l 18080 &
+poll() {
+  {
+    sleep 10
+    echo 'HTTP/1.0 200 OK'
+    echo 'Content-Type: text/html'
+    echo
+    echo 'OK'
+  } | nc -CNl 18080
+}
 
 cd ~/github
-git pull
-rsync -a --delete --exclude=.git/ ~/github/ ~/Dropbox/ICFPC2019/github
-git rev-parse HEAD > ~/nfs/GIT_COMMIT_ID
-git rev-parse HEAD > ~/Dropbox/ICFPC2019/GIT_COMMIT_ID
-wait
+while :; do
+  poll </dev/null &
+  sleep 5 &
+  git pull
+  rsync -a --delete --exclude=.git/ ~/github/ ~/Dropbox/ICFPC2019/github
+  git rev-parse HEAD > ~/nfs/GIT_COMMIT_ID
+  git rev-parse HEAD > ~/Dropbox/ICFPC2019/GIT_COMMIT_ID
+  wait
+done
