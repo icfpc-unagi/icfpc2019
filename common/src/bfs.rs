aca6cdf47cfa1c12ac0475ef0e07cdeeb575b03a
Takuya Akiba <t.akiba.65536@gmail.com>
2019-06-22 01:33:09 +0900

    bfs.rsを追加したよ

---
+++ common/src/bfs.rs
@@ -0,0 +1,31 @@
+use crate::*;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct BFS<'a> {
+    map: &'a Vec<Vec<Square>>,
+
+}
+
+impl<'a> BFS<'a> {
+    pub fn new(map: &Vec<Vec<Square>>) -> BFS {
+        BFS { map }
+    }
+
+    pub fn search_fewest_actions_to_move(
+        &mut self,
+        player_state: &PlayerState,
+        x: usize,
+        y: usize,
+    ) -> Vec<Action> {
+        unimplemented!();
+    }
+
+    pub fn search_fewest_actions_to_wrap(
+        &mut self,
+        player_state: &PlayerState,
+        x: usize,
+        y: usize,
+    ) -> Vec<Action> {
+        unimplemented!();
+    }
+}
