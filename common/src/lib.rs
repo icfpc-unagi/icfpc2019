f028d77e73dcc571ca44f48b0ee12df6ce74c5cf
Takuya Akiba <t.akiba.65536@gmail.com>
2019-06-21 21:24:56 +0900

    square booster dayoooooooo

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,20 +2,67 @@
 pub enum Square {
     Empty,
     Block,
+    Filled,
+}
+
+#[derive(Copy, Debug, Clone)]
+pub enum Booster {
     Extension,
     Fast,
     Drill,
     X,
 }
 
-pub fn read_map(path: &str) -> (Vec<Vec<Square>>, usize, usize) {
+/*
+#[derive(Copy, Debug, Clone)]
+pub struct PlayerState {
+    x: usize,  //・今いる座標
+    y: usize,
+    dir: usize,  //・向いている向き
+    unused_boosters: Vec<Square>,  //・持っている
+    active_boosters: Vec<(Square, i32)>,  //・発動中の効果、残りターン
+}
+
+fn parse_map(map: &str) -> Vec<(usize, usize)> {
+    let tokens: Vec<_> = map.split(',').collect();
+
+    for i in 0..tokens.len() / 2 {
+        let x = tokens[i * 2][1..];
+        let y = tokens[i * 2 + 1][..-1];
+        println!("{} {}", x, y);
+    }
+
+    unimplemented!();
+}
+
+fn parse_task(task: &str) -> (
+    Vec<(usize, usize)>,
+    (usize, usize),
+    Vec<Vec<(usize, usize)>>,
+    Vec<(Square, usize, usize)>,
+) {
+    let ss: Vec<_> = task.split('#').collect();
+    println!("task: {:?}", ss);
+
+    (
+        parse_map(ss[0]),
+        (0, 0),
+        vec![],
+        vec![],
+    )
+}
+*/
+
+pub fn read_task(path: &str) -> (Vec<Vec<Square>>, Vec<Vec<Option<Booster>>>, usize, usize) {
     /*
     let s = std::fs::read_to_string(path).unwrap();
     println!("{}", s);
 
-    let ss: Vec<str> = s.split('#').collect();
+    task = parse_task(&s);
+    println!("{:?}", task);
+
+    unimplemented!();
     */
-    /* unimplemented!();  */
 
     let (h, w) = (10, 10);
 
@@ -28,12 +75,17 @@ pub fn read_map(path: &str) -> (Vec<Vec<Square>>, usize, usize) {
         f[y][0] = Square::Block;
         f[y][w - 1] = Square::Block;
     }
-    return (f, 1, 1);
+    return (
+        f,
+        vec![vec![None; w]; h],
+        1,
+        1
+    );
 }
 
 #[cfg(test)]
 mod tests {
-    use super::read_map;
+    use super::read_task;
 
     #[test]
     fn it_works() {
@@ -43,6 +95,6 @@ mod tests {
 }
 
 fn main() {
-    let t = read_map("/Users/akiba/Downloads/part-1-initial/prob-001.desc");
+    let t = read_task("/Users/akiba/Downloads/part-1-initial/prob-001.desc");
     println!("{:?}", t);
 }
