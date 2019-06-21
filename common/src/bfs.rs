2c4711f0205fe18c17a782caac360c7cc331123b
Takuya Akiba <t.akiba.65536@gmail.com>
2019-06-22 04:28:10 +0900

    lambdaを受け取る感じでBFSをまたかなりリファクタリングした

--- common/src/bfs.rs
+++ common/src/bfs.rs
@@ -1,8 +1,9 @@
 use crate::*;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct BFS<'a> {
-    map: &'a Vec<Vec<Square>>,
+pub struct BFS {
+    xsize: usize,
+    ysize: usize,
     que_vec: Vec<(usize, usize)>,
     que_head: usize,
     pot: Vec<Vec<(usize, usize)>>,
@@ -10,15 +11,27 @@ pub struct BFS<'a> {
     is_goal: Vec<Vec<(usize)>>, // ここにこの向きで来ればゴール
 }
 
-impl<'a> BFS<'a> {
-    pub fn new(map: &Vec<Vec<Square>>) -> BFS {
+fn rotate((mut x, mut y): (i32, i32), mut dir: usize) {
+    // TODO: もう少しどっか広く使えそうな場所置く？
+    // TODO: 速くできる
+    while dir > 0 {
+        let (px, py) = (x, y);
+        x = y;
+        y = -x;
+        dir -= 0;
+    }
+}
+
+impl BFS {
+    pub fn new(xsize: usize, ysize: usize) -> BFS {
         BFS {
-            map,
+            xsize,
+            ysize,
             que_vec: vec![],
             que_head: 0,
-            pot: vec![vec![(!0, !0); map[0].len()]; map.len()],
+            pot: vec![vec![(!0, !0); xsize]; ysize],
             goals: vec![],
-            is_goal: vec![vec![!0; map[0].len()]; map.len()],
+            is_goal: vec![vec![!0; xsize]; ysize],
         }
     }
 
@@ -42,6 +55,9 @@ impl<'a> BFS<'a> {
 
     fn construct_actions(&mut self, mut x: usize, mut y: usize) -> Vec<Action> {
         let mut actions = vec![];
+
+        // self.is_goal[x][y]
+
         loop {
             let (c, d) = self.pot[x][y];
             if c == 0 {
@@ -57,7 +73,12 @@ impl<'a> BFS<'a> {
         actions
     }
 
-    fn search(&mut self, player_state: &PlayerState) -> Vec<Action> {
+    fn search<F: Fn(usize, usize) -> bool>(
+        &mut self,
+        map: &Vec<Vec<Square>>,
+        player_state: &PlayerState,
+        condition_func: F,
+    ) -> (Vec<Action>, usize, usize) {
         self.que_vec.push((player_state.x, player_state.y));
         self.pot[player_state.x][player_state.y].0 = 0;
 
@@ -69,7 +90,7 @@ impl<'a> BFS<'a> {
             y = self.que_vec[self.que_head].1;
             self.que_head += 1;
 
-            if self.is_goal[x][y] != !0 {
+            if condition_func(x, y) {
                 // eprintln!("{}", self.flg[x][y].0);
                 break;
             }
@@ -78,7 +99,7 @@ impl<'a> BFS<'a> {
             for d in 0..4 {
                 let (tx, ty) = apply_move((x, y), d);
 
-                if self.map[tx][ty] == Square::Block || self.pot[tx][ty].0 != !0 {
+                if map[tx][ty] == Square::Block || self.pot[tx][ty].0 != !0 {
                     continue;
                 }
 
@@ -87,30 +108,60 @@ impl<'a> BFS<'a> {
             }
         }
 
-        self.construct_actions(x, y)
+        (self.construct_actions(x, y), x, y)
+    }
+
+    pub fn search_fewest_actions_to_satisfy<F: Fn(usize, usize) -> bool>(
+        &mut self,
+        map: &Vec<Vec<Square>>,
+        player_state: &PlayerState,
+        condition_func: F,
+    ) -> (Vec<Action>, usize, usize) {
+        let ret = self.search(map, player_state, condition_func);
+        self.clean_up();
+        ret
     }
 
     pub fn search_fewest_actions_to_move(
         &mut self,
+        map: &Vec<Vec<Square>>,
         player_state: &PlayerState,
         target_x: usize,
         target_y: usize,
     ) -> Vec<Action> {
         self.add_goal(target_x, target_y, player_state.dir);
-        let actions = self.search(player_state);
+        let mut is_goal = vec![];
+        std::mem::swap(&mut is_goal, &mut self.is_goal);
+        let f = |x: usize, y: usize| is_goal[x][y] != !0;
+        let (actions, _, _) = self.search(map, player_state, f);
+        std::mem::swap(&mut is_goal, &mut self.is_goal);
         self.clean_up();
         actions
     }
 
     pub fn search_fewest_actions_to_wrap(
         &mut self,
+        map: &Vec<Vec<Square>>,
         player_state: &PlayerState,
         target_x: usize,
         target_y: usize,
     ) -> Vec<Action> {
-
-
         unimplemented!();
+
+        /*
+        for (mx, my) in player_state.manipulators.iter() {
+            for d in 0..4 {
+                let (dx, dy) = rotate((*mx, *my), (d + 2) % 4);
+                let (tx, ty) = (target_x + dx, target_y + dy);
+                if self.xsize <= tx || self.ysize <= ty {
+                    continue;
+                }
+                self.add_goal(tx, ty, d);
+            }
+        }
+
+        // TODO: ここで回転する
+        */
     }
 }
 
@@ -136,13 +187,13 @@ mod tests {
             }
         };
 
-        let mut bfs = BFS::new(&map);
+        let mut bfs = BFS::new(map.len(), map[0].len());
         for _ in 0..100 {
             let (sx, sy) = random_empty_cell();
             let (tx, ty) = random_empty_cell();
 
             let mut ps = PlayerState::new(sx, sy);
-            let actions = bfs.search_fewest_actions_to_move(&ps, tx, ty);
+            let actions = bfs.search_fewest_actions_to_move(&map, &ps, tx, ty);
 
             for a in actions.iter() {
                 ps.apply_action(*a);
