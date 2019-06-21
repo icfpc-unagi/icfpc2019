cbdf2ff6284d2ad5321b3beb78d760f39af902d0
Takuya Akiba <t.akiba.65536@gmail.com>
2019-06-22 00:55:07 +0900

    とりあえずbooster完全無視で移動回転についてapply_actionが動くようになった気がする

---
+++ common/src/player_state.rs
@@ -0,0 +1,98 @@
+use crate::*;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct PlayerState {
+    x: usize,  //・今いる座標
+    y: usize,
+    dir: usize,  //・向いている向き
+    unused_boosters: Vec<Booster>,  //・持っている
+    active_boosters: Vec<(Booster, i32)>,  //・発動中の効果、残りターン
+    manipulators: Vec<(i32, i32)>,  // マニピュレータたちの相対位置（方向0のときの）
+}
+
+impl PlayerState {
+    pub fn apply_action(&mut self, action: Action) {
+        match action {
+            Action::Move(dir) => {
+                // TODO: Fastがonだったら2マス移動したりする
+                let (x, y) = apply_move((self.x, self.y), dir);
+                self.x = x;
+                self.y = y;
+            }
+            Action::Nothing =>
+                (),
+            Action::TurnR => {
+                self.dir += 1;
+                self.dir %= 4;
+
+                for m in self.manipulators.iter_mut() {
+                    let p = *m;
+                    m.0 = p.1;
+                    m.1 = -p.0;
+                }
+            }
+            Action::TurnL => {
+                self.dir += 3;
+                self.dir %= 4;
+
+                for m in self.manipulators.iter_mut() {
+                    let p = *m;
+                    m.0 = -p.1;
+                    m.1 = p.0;
+                }
+            }
+            Action::Extension(dx, dy) =>
+                self.manipulators.push((dx, dy)),
+            Action::Fast =>
+            // TODO: unused_boostersからfastを除いてactive_boostersに追加
+                unimplemented!(),
+            Action::Drill =>
+            // TODO: マップに対する作用は一体？？？
+                unimplemented!(),
+        }
+        // TODO: 発動中の効果の有効期限を減らしたりする
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let a = PlayerState {
+            x: 10,
+            y: 20,
+            dir: 0,
+            unused_boosters: vec![],
+            active_boosters: vec![],
+            manipulators: vec![(1, 0), (1, 1), (1, -1)],
+        };
+
+        let mut b = a.clone();
+        b.apply_action(Action::Move(0));
+        assert_eq!(b, PlayerState{x: 11, .. a.clone()});
+        // dbg!(b);
+
+        let mut b = a.clone();
+        b.apply_action(Action::TurnR);
+        assert_eq!(b, PlayerState{
+            dir: 1,
+            manipulators: vec![(
+                                   0,
+                                   -1,
+                               ),
+                               (
+                                   1,
+                                   -1,
+                               ),
+                               (
+                                   -1,
+                                   -1,
+                               ),],
+            .. a.clone()
+        });
+        // dbg!(b);
+    }
+}
+
