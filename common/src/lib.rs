bda4926b9e4cccb3c31655271abd027b6147caf2
Takuya Akiba <t.akiba.65536@gmail.com>
2019-06-21 22:27:56 +0900

    booster以外読めるようになった気がする

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -13,8 +13,7 @@ pub enum Booster {
     X,
 }
 
-/*
-#[derive(Copy, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct PlayerState {
     x: usize,  //・今いる座標
     y: usize,
@@ -23,47 +22,142 @@ pub struct PlayerState {
     active_boosters: Vec<(Square, i32)>,  //・発動中の効果、残りターン
 }
 
-fn parse_map(map: &str) -> Vec<(usize, usize)> {
-    let tokens: Vec<_> = map.split(',').collect();
+////////////////////////////////////////////////////////////////////////////////
+// Parse
+////////////////////////////////////////////////////////////////////////////////
 
-    for i in 0..tokens.len() / 2 {
-        let x = tokens[i * 2][1..];
-        let y = tokens[i * 2 + 1][..-1];
-        println!("{} {}", x, y);
-    }
-
-    unimplemented!();
+#[derive(Debug, Clone)]
+struct TaskSpecification {
+    frame: Vec<(usize, usize)>,
+    initial_location: (usize, usize),
+    obstacles: Vec<Vec<(usize, usize)>>,
+    boosters: Vec<(Booster, usize, usize)>,
 }
 
-fn parse_task(task: &str) -> (
-    Vec<(usize, usize)>,
-    (usize, usize),
-    Vec<Vec<(usize, usize)>>,
-    Vec<(Square, usize, usize)>,
-) {
+fn parse_point_tokens(x: &str, y: &str) -> (usize, usize) {
+    let x = &x[1..];
+    let y = &y[..y.len() - 1];
+    (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
+}
+
+fn parse_map(s: &str) -> Vec<(usize, usize)> {
+    let ts: Vec<_> = s.split(',').collect();
+
+    (0..ts.len() / 2).map(|i| {
+        parse_point_tokens(&ts[i *  2], &ts[i * 2 + 1])
+    }).collect()
+}
+
+fn parse_point(s: &str) -> (usize, usize) {
+    let ts: Vec<_> = s.split(',').collect();
+    parse_point_tokens(&ts[0], &ts[1])
+}
+
+fn parse_task(task: &str) -> TaskSpecification {
     let ss: Vec<_> = task.split('#').collect();
     println!("task: {:?}", ss);
 
+    TaskSpecification {
+        frame: parse_map(ss[0]),
+        initial_location: parse_point(ss[1]),
+        obstacles: vec![],
+        boosters: vec![],
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Rasterize
+////////////////////////////////////////////////////////////////////////////////
+
+fn get_size(task: &TaskSpecification) -> (usize, usize) {
     (
-        parse_map(ss[0]),
-        (0, 0),
-        vec![],
-        vec![],
+        task.frame.iter().map(|&p| p.0).max().unwrap() + 2,
+        task.frame.iter().map(|&p| p.1).max().unwrap() + 2,
     )
 }
-*/
+
+fn draw_contour(accsum: &mut Vec<Vec<i32>>, contour: &Vec<(usize, usize)>) {
+    for i in 0..contour.len() {
+        let p1 = contour[i];
+        let p2 = contour[(i + 1) %  contour.len()];
+
+        if p1.1 == p2.1 {
+            let y = p1.1;
+            let xmin = usize::min(p1.0, p2.0);
+            let xmax = usize::max(p1.0, p2.0);
+            for x in xmin..xmax {
+                let x = x + 1;
+                let y = y + 1;
+                assert_eq!(accsum[x][y], 0);
+                accsum[x][y] = 1;
+            }
+        }
+    }
+}
+
+fn accsum_to_squares(accsum: &mut Vec<Vec<i32>>) -> Vec<Vec<Square>> {
+    let xsize = accsum.len();
+    let ysize = accsum[0].len();
+    for x in 0..xsize {
+        for y in 1..ysize {
+            accsum[x][y] += accsum[x][y - 1];
+        }
+    }
+    println!("{:?}", accsum);
+
+    accsum.iter().map(|row| {
+        row.iter().map(|c| {
+            if c % 2 == 0 {
+                Square::Block
+            } else {
+                Square::Empty
+            }
+        }).collect()
+    }).collect()
+}
+
+fn debug_map(map: &Vec<Vec<Square>>) {
+    let xsize = map.len();
+    let ysize = map[0].len();
+
+    for y in (0..ysize).rev() {
+        print!("{:02}:", y);
+        for x in 0..xsize {
+            print!("{}", match map[x][y] {
+                Square::Empty => ' ',
+                Square::Block => '#',
+                Square::Filled => '.',
+            })
+        }
+        println!();
+    }
+}
 
 pub fn read_task(path: &str) -> (Vec<Vec<Square>>, Vec<Vec<Option<Booster>>>, usize, usize) {
-    /*
     let s = std::fs::read_to_string(path).unwrap();
-    println!("{}", s);
-
-    task = parse_task(&s);
+    let task = parse_task(&s);
     println!("{:?}", task);
 
-    unimplemented!();
-    */
+    let (xsize, ysize) = get_size(&task);
+    println!("{} {}", xsize, ysize);
 
+    let mut accsum = vec![vec![0; ysize]; xsize];
+    draw_contour(&mut accsum, &task.frame);
+    println!("{:?}", accsum);
+
+    let squares = accsum_to_squares(&mut accsum);
+    println!("{:?}", squares);
+
+    debug_map(&squares);
+
+    (
+        squares,
+        vec![vec![None; ysize]; xsize],
+        task.initial_location.0 + 1,
+        task.initial_location.1 + 1,
+    )
+
+    /*
     let (h, w) = (10, 10);
 
     let mut f = vec![vec![Square::Empty; w]; h];
@@ -81,6 +175,7 @@ pub fn read_task(path: &str) -> (Vec<Vec<Square>>, Vec<Vec<Option<Booster>>>, us
         1,
         1
     );
+    */
 }
 
 #[cfg(test)]
@@ -94,7 +189,3 @@ mod tests {
     }
 }
 
-fn main() {
-    let t = read_task("/Users/akiba/Downloads/part-1-initial/prob-001.desc");
-    println!("{:?}", t);
-}
