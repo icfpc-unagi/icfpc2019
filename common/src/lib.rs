dfb6fd29c99fa9c9a35d5a88ec84154a1f49f3b0
Yoichi Iwata <yiwata@nii.ac.jp>
2019-06-22 00:11:17 +0900

    setmin

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -11,6 +11,19 @@ macro_rules! mat {
 	($e:expr; $d:expr $(; $ds:expr)+) => { Vec::from(vec![mat![$e $(; $ds)*]; $d]) };
 }
 
+pub trait SetMinMax {
+	fn setmin(&mut self, v: Self) -> bool;
+	fn setmax(&mut self, v: Self) -> bool;
+}
+impl<T> SetMinMax for T where T: PartialOrd {
+	fn setmin(&mut self, v: T) -> bool {
+		*self > v && { *self = v; true }
+	}
+	fn setmax(&mut self, v: T) -> bool {
+		*self < v && { *self = v; true }
+	}
+}
+
 #[derive(Copy, Debug, Clone, PartialEq, Eq)]
 pub enum Square {
     Empty,
