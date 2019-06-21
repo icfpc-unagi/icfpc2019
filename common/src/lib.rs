33c35d255d503150ac016e58b6e2da0124995b2c
Yoichi Iwata <yiwata@nii.ac.jp>
2019-06-21 22:19:34 +0900

    Action

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -23,6 +23,43 @@ pub enum Booster {
     X,
 }
 
+pub fn apply_move((x, y): (usize, usize), dir: usize) -> (usize, usize) {
+    match dir {
+        0 => (x + 1, y),
+        1 => (x, y - 1),
+        2 => (x - 1, y),
+        3 => (x, y + 1),
+        _ => panic!("illegal dir: {}", dir)
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Action {
+    Move(usize),
+    Nothing,
+    TurnR,
+    TurnL,
+    Extension(i32, i32),
+    Fast,
+    Drill
+}
+
+pub fn output(list: &Vec<Action>) -> String {
+    let mut out = String::new();
+    for mv in list {
+        match mv {
+            Action::Move(dir) => out += ["D", "S", "A", "W"][*dir],
+            Action::Nothing => out += "Z",
+            Action::TurnR => out += "E",
+            Action::TurnL => out += "Q",
+            Action::Extension(dx, dy) => out += &format!("B({},{})", dx, dy),
+            Action::Fast => out += "F",
+            Action::Drill => out += "L"
+        }
+    }
+    out
+}
+
 /*
 #[derive(Copy, Debug, Clone)]
 pub struct PlayerState {
