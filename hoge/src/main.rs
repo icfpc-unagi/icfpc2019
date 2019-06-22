7d9b30e3460bdb7ae29da02392cead0c10a5a367
Takuya Akiba <t.akiba.65536@gmail.com>
2019-06-23 02:11:11 +0900

    hoge

---
+++ hoge/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let task = common::read_task("../data/part-3-clones-examples/example-03-1.desc");
+    let sol = common::read_sol("../data/part-3-clones-examples/example-03-1.sol");
+    common::insert_fast(task, sol);
+}
