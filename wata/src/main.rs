c606d76bad0e839525cbea27ad543084a63341ee
Yoichi Iwata <yiwata@nii.ac.jp>
2019-06-22 18:45:18 +0900

    clone+manipulate

--- wata/src/main.rs
+++ wata/src/main.rs
@@ -267,6 +267,47 @@ fn solve(map: &Vec<Vec<Square>>, boosters: &Vec<Vec<Option<Booster>>>, (sx, sy):
     actions
 }
 
+fn chokudai_main(t: RasterizedTask, op: usize) -> Vec<Action> {
+    let mut best_action = vec![];
+    let mut loop_cnt = 0;
+    loop{
+        let initialMove = bootstrap_expand_1_migimae(&t, loop_cnt);
+        if loop_cnt + 4 > (initialMove.2).manipulators.len() {
+            break;
+        }
+        loop_cnt += 1;
+
+        let (first_field, first_itemfield, FX, FY) = t.clone();
+        let H = first_field.len();
+        let W = first_field[0].len();
+        let default_field = first_field.clone();
+
+        let first_state = chokudai::get_first_state(first_field, first_itemfield, FX, FY);
+        
+        //途中で塗られたものを使用するバージョン
+        //let second_state = get_first_state((initialMove.0).0, (initialMove.0).1, (initialMove.2).x, (initialMove.2).y);
+        //途中で塗られたものを使用しないバージョン
+        let mut second_state = chokudai::get_first_state(default_field, (initialMove.0).1, (initialMove.2).x, (initialMove.2).y);
+        eprintln!("{}", second_state.p.manipulators.len());
+        second_state.p.manipulators = (initialMove.2).manipulators;
+        eprintln!("{}", second_state.p.manipulators.len());
+
+        //let mut final_action = make_action_by_state(&first_state, 1);
+        let mut final_action = chokudai::make_action_by_state(&second_state, op);
+        
+        let pre_action = initialMove.1.clone();
+        let ans_action = final_action.clone();
+
+        let size =  pre_action.len() + ans_action.len();
+        eprintln!("add: {} size: {}", loop_cnt, size);
+        if best_action.len() == 0 || best_action.len() > size{
+            best_action = pre_action.clone();
+            best_action.extend(ans_action);
+        }
+    }
+    best_action
+}
+
 fn clone_solve(map: &Vec<Vec<Square>>, boosters: &Vec<Vec<Option<Booster>>>, (sx, sy): (usize, usize)) -> Vec<Vec<Action>> {
     let n = map.len();
     let m = map[0].len();
@@ -310,8 +351,9 @@ fn clone_solve(map: &Vec<Vec<Square>>, boosters: &Vec<Vec<Option<Booster>>>, (sx
             sy -= dy;
             let mut best_move = vec![];
             for op in 0..2 {
-                let ch_state = chokudai::get_first_state(map.clone(), boosters.clone(), sx, sy);
-                let mv = chokudai::make_action_by_state(&ch_state, op);
+                let mv = chokudai_main((map.clone(), boosters.clone(), sx, sy), op);
+                // let ch_state = chokudai::get_first_state(map.clone(), boosters.clone(), sx, sy);
+                // let mv = chokudai::make_action_by_state(&ch_state, op);
                 if op == 0 || best_move.len() > mv.len() {
                     best_move = mv;
                 }
