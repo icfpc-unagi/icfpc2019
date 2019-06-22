18f33176b897a11ab21390ecc9a0f169fc18dbf6
chokudai <chokudai@gmail.com>
2019-06-22 17:29:23 +0900

    動くようにした

--- chokudai/icfpc2019/src/main.rs
+++ chokudai/icfpc2019/src/main.rs
@@ -5,16 +5,26 @@ fn main() {
     let taskfile = std::env::args().nth(1).expect("usage: args[1] = taskfile");
     let t = read_task(&taskfile);
 
-    let initialMove = bootstrap_expand(&t);
+    //let initialMove = bootstrap_expand(&t);
     
     let (first_field, first_itemfield, FX, FY) = t;
     let H = first_field.len();
     let W = first_field[0].len();
+    let default_field = first_field.clone();
 
     let first_state = get_first_state(first_field, first_itemfield, FX, FY);
     
+    //途中で塗られたものを使用するバージョン
+    //let second_state = get_first_state((initialMove.0).0, (initialMove.0).1, (initialMove.2).x, (initialMove.2).y);
+    //途中で塗られたものを使用しないバージョン
+    //let second_state = get_first_state(default_field, (initialMove.0).1, (initialMove.2).x, (initialMove.2).y);
+    
     let mut final_action = make_action_by_state(&first_state, true);
-
+    //let mut final_action = make_action_by_state(&second_state, true);
+    
+    //let pre_string = actions_to_string(&initialMove.1);
     let ans_string = actions_to_string(&final_action);
+
+    //println!("{}{}", pre_string, ans_string);
     println!("{}", ans_string);
 }
