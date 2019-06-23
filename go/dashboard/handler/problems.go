15642ae2d01f94ed9b5a9ab9f0b94c45cd9f413f
sulume <sulume@gmail.com>
2019-06-23 23:45:38 +0900

    problem metadata

--- go/dashboard/handler/problems.go
+++ go/dashboard/handler/problems.go
@@ -3,7 +3,11 @@ package handler
 import (
 	"context"
 	"fmt"
+	"html/template"
 	"net/http"
+	"strings"
+
+	"github.com/imos/icfpc2019/go/util/metadata"
 
 	"github.com/imos/icfpc2019/go/util/db"
 )
@@ -14,30 +18,38 @@ func init() {
 
 func problemsHandler(ctx context.Context, r *http.Request) (HTML, error) {
 	problems := []struct {
-		ProblemID      int64  `db:"problem_id"`
-		ProblemName    string `db:"problem_name"`
-		ProblemCreated string `db:"problem_created"`
+		ProblemID       int64  `db:"problem_id"`
+		ProblemName     string `db:"problem_name"`
+		ProblemDataBlob string `db:"problem_data_blob"`
+		ProblemCreated  string `db:"problem_created"`
 	}{}
 	if err := db.Select(ctx, &problems,
-		`SELECT problem_id, problem_name, problem_created `+
-			`FROM problems ORDER BY problem_name`); err != nil {
+		`SELECT problem_id, problem_name, problem_data_blob, problem_created `+
+			`FROM problems NATURAL JOIN problem_data  ORDER BY problem_name`); err != nil {
 		return "", err
 	}
-	output := HTML(
+	sb := &strings.Builder{}
+	sb.WriteString(
 		`<table class="table table-clickable">` +
-			`<thead><tr><td>ID</td><td>Name</td><td>Created</td></thead>` +
+			`<thead><tr><td>ID</td><td width="400">Image</td><td>Name</td><td>Size</td><td>Boosters</td><td>Created</td></thead>` +
 			`<tbody>`)
 
 	for _, problem := range problems {
-		id := Escape(fmt.Sprintf("%d", problem.ProblemID))
-		output += "<tr><td>" +
-			id +
-			`</td><td><img src="/problem_image?problem_id=` + id + `">` +
-			Escape(problem.ProblemName) +
-			"</a></td><td>" +
-			Escape(problem.ProblemCreated) +
-			"</td></tr>"
+		md, err := metadata.GetTaskMetadata(problem.ProblemDataBlob)
+		if err != nil {
+			return "", err
+		}
+		id := fmt.Sprintf("%d", problem.ProblemID)
+		sb.WriteString("<tr><td>")
+		sb.WriteString(id)
+		sb.WriteString(`</td><td><img src="/problem_image?problem_id=`)
+		sb.WriteString(id)
+		sb.WriteString(`" style="max-width:400px;height:auto"></td><td>`)
+		sb.WriteString(template.HTMLEscapeString(problem.ProblemName))
+		fmt.Fprintf(sb, `</td><td>(%v,%v)</td><td><code>%v</code></td><td>`, md.MaxX, md.MaxY, md.Boosters)
+		sb.WriteString(template.HTMLEscapeString(problem.ProblemCreated))
+		sb.WriteString("</td></tr>")
 	}
-	output += `</tbody></table>`
-	return output, nil
+	sb.WriteString(`</tbody></table>`)
+	return HTML(sb.String()), nil
 }
