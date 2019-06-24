031cf7bcd3201043629b5a4cc994098b317eaff0
Takuya Akiba <t.akiba.65536@gmail.com>
2019-06-24 14:24:18 +0900

    手動用にoptimize_pure_moveを提供

--- hoge/src/main.rs
+++ hoge/src/main.rs
@@ -9,10 +9,12 @@ fn main() {
 
     let optimized_actions = common::optimize_pure_move_old(&task, original_actions);
     let (n1, n2) = (original_actions.len(), optimized_actions.len());
+    /*
     println!(
         "{} -> {} ({:.2}%)",
         n1, n2,
         ((n1 - n2) as f32) / (n1 as f32) * 100.0
     );
-    //println!("{}", common::actions_to_string(&optimized_actions));
+    */
+    println!("{}", common::actions_to_string(&optimized_actions));
 }
